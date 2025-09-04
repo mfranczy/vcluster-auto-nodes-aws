e54b1484dea1c77d8a1e43fa3c92dd151430cf21
Dmytro Sydorov <dmytro.sydorov@loft.sh>
2025-09-04 10:48:00 +0200

    chore: refactor modules

--- environment/outputs.tf
+++ environment/outputs.tf
@@ -1,7 +1,14 @@
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "private_subnet_ids" {
+  description = "A list of private subnet ids"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnet_ids" {
+  description = "A list of public subnet ids"
+  value       = module.vpc.public_subnets
 }
 
 output "security_group_id" {
-  value = aws_security_group.instance_sg.id
+  description = "Security group id to attach to worker nodes"
+  value       = aws_security_group.workers.id
 }
