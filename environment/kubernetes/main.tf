f74a373d6706049ad4c5d4b85e462a3f939726be
Marcin Franczyk <marcin0franczyk@gmail.com>
2025-09-24 12:12:01 +0200

    add kubernetes test
    Signed-off-by: Marcin Franczyk <marcin0franczyk@gmail.com>

---
+++ environment/kubernetes/main.tf
@@ -0,0 +1,39 @@
+# test
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx"
+    namespace = default
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:1.25-alpine"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
