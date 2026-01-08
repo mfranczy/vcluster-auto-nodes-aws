b903b41f82e769a2194cfd92b9a20f7ce1bee57a
Marcin Franczyk <marcin0franczyk@gmail.com>
2026-01-08 11:46:48 +0100

    fix: ignore empty user data
    Signed-off-by: Marcin Franczyk <marcin0franczyk@gmail.com>

--- node/locals.tf
+++ node/locals.tf
@@ -5,7 +5,7 @@ locals {
   subnet_id             = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["private_subnet_ids"][random_integer.subnet_index.result])
   instance_type         = nonsensitive(var.vcluster.nodeType.spec.properties["instance-type"])
   security_group_id     = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["security_group_id"])
-  user_data             = var.vcluster.userData
+  user_data             = var.vcluster.userData != "" ? var.vcluster.userData : null
   instance_profile_name = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["instance_profile_name"])
   cluster_tag           = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["cluster_tag"])
 }
